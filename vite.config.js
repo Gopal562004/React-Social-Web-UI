02618f403b06f77ad352d12edb86a2ec93615eb4
Gopal562004 <gopalgawas2004@gmail.com>
2024-06-23 22:05:45 +0530

    u

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Social-Web-UI/",
-  server: {
-    middlewareMode: "html",
-    setupMiddlewares: (middlewares) => {
-      middlewares.use((req, res, next) => {
-        res.setHeader("Permissions-Policy", "interest-cohort=()");
-        next();
-      });
-      return middlewares;
-    },
-  },
+  base: "/React-Social-Web-UI/", // Replace with your repository name
 });
