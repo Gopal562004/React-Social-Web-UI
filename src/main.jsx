b826f3409928ec83f983167d70f4f40ad0fe29f5
Gopal562004 <gopalgawas2004@gmail.com>
2024-06-23 22:21:11 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./routes/App.jsx";
+import React from "react"; // Add this line
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./routes/App.jsx";
 import Createpost from "./components/Createpost.jsx";
 import Postlist from "./components/Postlist.jsx";
+import ReactDOM from "react-dom";
 
-// const router = (
-//   <Router>
-//     <Routes>
-//       <Route path="/" element={<App />} />
-//       <Route path="/create-post" element={<Createpost />} />
-//     </Routes>
-//   </Router>
-// );
+
 const router = (
-  <Router>
+  <Router basename="/React-Social-Web-UI">
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="/" element={<Postlist />} />
+        <Route index element={<Postlist />} />
         <Route path="/create-post" element={<Createpost />} />
       </Route>
     </Routes>
   </Router>
 );
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>{router}</React.StrictMode>
 );
