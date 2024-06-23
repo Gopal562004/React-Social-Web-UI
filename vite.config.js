f2ecfeed17e3504768a58efa82fc6f6a2a81795e
Gopal562004 <gopalgawas2004@gmail.com>
2024-06-23 16:38:37 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/React-Social-UI/", // Adjust the base path according to your GitHub Pages URL
+});
